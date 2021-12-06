38f57177ab091137696855e3ab1d1e42e15bc336
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2021-12-06 08:52:56 +0100

    day06

---
+++ lib/aoc_2021/day06/solver_test.exs
@@ -0,0 +1,28 @@
+defmodule AOC2021.Day06.SolverTest do
+  use ExUnit.Case
+
+  alias AOC2021.Day06.Solver, as: MUT
+
+  describe "solve/2" do
+    @tag :day06
+    test "should solve first task with first test input from website correctly" do
+      stream = """
+      3,4,3,1,2
+      """
+      |> String.split("\n", trim: true)
+
+      assert 5934 == MUT.solve(stream, :first)
+    end
+
+    @tag :day06
+    test "should solve second task with first test input from website correctly" do
+      stream = """
+      3,4,3,1,2
+      """
+      |> String.split("\n", trim: true)
+
+      assert 26984457539 == MUT.solve(stream, :second)
+
+    end
+  end
+end
